c9b7071ec7292cbf6ffe91f2e03e7f15dfac1b97
Asogwa Chukwuchebem <chukwuchebemasogwa@gmail.com>
2024-08-14 01:59:03 +0100

    learning react hooks

--- app/page.js
+++ app/page.js
@@ -1,10 +1,17 @@
-
-
+import { Todo } from "/components/Todo"
+import { TallyCounter } from "../components/TallyCounter"
+import { Random } from "@/components/Random"
 export default function Home () {
   return (
-    <div>
-      <h1>Hello world</h1>
-      <p>Another line of code</p>
+    <div className="h-screen flex justify-center items-center bg-white" >
+      <div className="h-full w-[400px] border border-gray-300 p-4">
+        {/* /* <Todo bg='blue' border='green'> */}
+        {/* <span>call dad</span>      */}
+        {/* <span>14:20</span> */}
+        {/* </Todo> */}
+        {/* // <TallyCounter/> */ }
+        <Random/>
+      </div>
     </div>
   )
 }
